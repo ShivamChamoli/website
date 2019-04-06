b770b0db5d3a147a4c1f10b87f67821ecb1d8d98
Shivam Chamoli <dhruvchamoli@gmail.com>
2019-04-06 12:05:15 -0400

    founder director's image

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/Aboutus.css';
 import {Row, Col} from 'react-bootstrap';
-import logo from '../images/logo.svg';
+import founderDirector from '../images/mom.png';
 //import withRouter from 'react-router-dom/withRouter';
 
 export default class Aboutus extends React.Component {
@@ -50,14 +50,14 @@ export default class Aboutus extends React.Component {
                 DIRECTOR’S PROFILE 
             </h4>
             <Row>
-            <Col xs={9} lg={9} md={9}>Mrs. Seema Chamoli
+            <Col xs={8} lg={8} md={8}>Mrs. Seema Chamoli
                     the founder director of Woodsorrel, Angels' Meadow, is a <b>reputed educationist and has worked with children her whole career</b> and earned invaluable experience in the teaching and administrative field.
                     She is <b>thoughtful, compassionate and highly skilled at working with young children</b>. She approaches <b>each child as an individual</b> and learns about where they are <b>emotionally and developmentally</b> so that she can best facilitate their learning. 
                     The most important areas of her <b>specializations include 'Childhood Education and Nutrition'</b>. Childhood Education emerged as an interest, evolved as a passion and took over all other interests of Mrs. Seema Chamoli.
                     She has <b>experience in private, public and hi-tech preschool settings</b>. She has worked in various Schools and in the best Pre-schools of India (ranging from ...).  She has also worked as VP educational projects (...).
                     <b>Realizing the necessity of world class Pre-school in Dehra Dun</b>, she nurtured the <b>concept of Woodsorrel Angels Meadow which is a blend of modernity and traditions</b>. It aims to <b>prepare the children for success in today’s competitive environment and be an excellent global citizen</b>.
                 </Col>
-                <Col xs={3} lg={3} md={3}> <img src={logo} className="img-responsive img-circle" alt="founder director" /> </Col>
+                <Col xs={4} lg={4} md={4}> <img style={{background:'lightgrey'}} src={founderDirector} className="img-responsive img-thumbnail" alt="founder director" /> </Col>
             </Row>
         </div>
       );
